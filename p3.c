b40f89c11cd356921cd6782846a7e0613e0d3133
Constantin A. Tudorica <tudalex@gmail.com>
2012-11-26 23:20:14 +0200

    Modificat 3

--- p3.c
+++ p3.c
@@ -7,13 +7,16 @@ void solve(int *a, int poz, int size) {
 	solve(a, poz+2, size); // Apelam recursiv, crescand poz cu 2
 
 }
+void citire(int *a, int *n) {
+	int i;
+	scanf("%d ", n); // Citesc numarul de elemente
+	for (i = 0; i < *n; ++ i)
+		scanf("%d", &a[i]);
+}
 int main() {
 	int i, n;
-	int *a = (int*)malloc(sizeof(int)*128); //Creeam un vector cu 128 de elemnte, defapt alocam memorie pentru el
-
-	scanf("%d ", &n); // Citesc numarul de elemente
-	for (i = 0; i < n; ++ i)
-		scanf("%d", &a[i]);
+	int a[128]; 
+	citire(a,&n);
 	solve(a,0,n);
 	printf("\n");
 	return 0;
